ef805770f3288a8bc30e9a6b7303133091a1cf0c
pete.ladwig <pete.ladwig@singlewire.com>
2025-02-20 15:00:03 -0600

    adding image tests

--- src/test/java/persistence/ImageDaoTest.java
+++ src/test/java/persistence/ImageDaoTest.java
@@ -1,5 +1,8 @@
 package persistence;
 
+import entity.Glaze;
+import entity.Image;
+import entity.Project;
 import entity.User;
 import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.BeforeAll;
@@ -8,15 +11,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import util.Database;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
- * Test class for UserDao
+ * Test class for ImageDao
  */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ImageDaoTest {
 
-    private static GenericDao<User> userDao;
+    private static GenericDao<Image> dao;
     private static SessionFactory sessionFactory;
-    private User testUser;
+    private Image testResource;
 
     /**
      * Set up prior to spec running
@@ -24,11 +33,11 @@ class ImageDaoTest {
     @BeforeAll
     static void setup() {
         sessionFactory = SessionFactoryProvider.getSessionFactory();
-        userDao = new GenericDao<>(User.class);
+        dao = new GenericDao<>(Image.class);
     }
 
     /**
-     * create a test user before each test
+     * create a test resource before each test
      */
     @BeforeEach
     void setupEach() {
@@ -37,7 +46,7 @@ class ImageDaoTest {
         database.runSQL("cleanDB.sql");
 
         //create one user using sql
-        database.runSQL("createUser.sql");
+        database.runSQL("createTestResources.sql");
 
 
     }
@@ -48,7 +57,17 @@ class ImageDaoTest {
      */
     @Test
     void testInsertResource() {
+        //create data for test tag
+        GenericDao<Project> projectDao = new GenericDao<>(Project.class);
+        Project projectToAdd = projectDao.getById(1);
+        testResource = new Image(0, projectToAdd, "www.imageurl.com", Timestamp.from(Instant.now()));
 
+        //insert
+        testResource.setImageId(dao.insert(testResource));
+        assertEquals(2, testResource.getImageId(), "id should be 2 after insertion!");
+        Image insertedResource = dao.getById(testResource.getImageId());
+        assertNotNull(insertedResource, "new resource should be retrievable");
+        assertEquals("www.imageurl.com", insertedResource.getImageUrl(), "name should match");
     }
 
     /**
@@ -56,7 +75,10 @@ class ImageDaoTest {
      */
     @Test
     void testGetById() {
-
+        //retrieving tag added by insertion
+        testResource = dao.getById(1);
+        assertNotNull(testResource, "resource should be able to be found by id");
+        assertEquals("https://example.com/images/project1.jpg", testResource.getImageUrl(), "name should match");
     }
 
     /**
@@ -64,7 +86,13 @@ class ImageDaoTest {
      */
     @Test
     void testUpdateResource() {
+        testResource = dao.getById(1);
+        testResource.setImageUrl("www.updatedImageUrl.com");
+        dao.saveOrUpdate(testResource);
 
+        Image updatedResource = dao.getById(testResource.getImageId());
+        assertNotNull(updatedResource, "updated resource shouldn't be null");
+        assertEquals("www.updatedImageUrl.com", testResource.getImageUrl(), "resource should be updated");
     }
 
     /**
@@ -72,15 +100,20 @@ class ImageDaoTest {
      */
     @Test
     void testDeleteResource() {
-
+        testResource = dao.getById(1);
+        dao.delete(testResource);
+        Image deletedResource = dao.getById(testResource.getImageId());
+        assertNull(deletedResource, "resource should be deleted");
     }
 
     /**
-     * Get all users check
+     * Get all check
      */
     @Test
     void testGetAllResources() {
-
+        testResource = dao.getById(1);
+        List<Image> resourceList = dao.getAll();
+        assertFalse(resourceList.isEmpty(), "list shouldn't be empty");
     }
 
     /**
@@ -88,7 +121,10 @@ class ImageDaoTest {
      */
     @Test
     void testGetByPropertyEqual() {
-
+        testResource = dao.getById(1);
+        List<Image> resourceList = dao.getByPropertyEqual("imageUrl", "https://example.com/images/project1.jpg");
+        assertFalse(resourceList.isEmpty(), "should return at least one resource");
+        assertEquals("https://example.com/images/project1.jpg", resourceList.get(0).getImageUrl(), "value should match");
     }
 
     /**
@@ -96,6 +132,9 @@ class ImageDaoTest {
      */
     @Test
     void testGetByPropertyLike() {
-
+        testResource = dao.getById(1);
+        List<Image> resourceList = dao.getByPropertyLike("imageUrl", "https://example.com/images/project1.jpg");
+        assertFalse(resourceList.isEmpty(), "should return users with 'example' in resource");
+        assertTrue(resourceList.get(0).getImageUrl().contains("example"), "value should contain 'example'");
     }
 }
