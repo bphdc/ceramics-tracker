b1720c2d50bfd1b3f9dd6c27ab0aeb37ac487ab0
pete.ladwig <pete.ladwig@singlewire.com>
2025-02-20 09:50:00 -0600

    Adding images and project entries entities

--- src/main/java/entity/Images.java
+++ src/main/java/entity/Images.java
@@ -1,4 +1,119 @@
 package entity;
 
+import java.sql.Timestamp;
+
+/**
+ * The type Images.
+ */
 public class Images {
+
+
+    private int image_id;
+
+    private int project_id;
+
+    private String image_url;
+
+    private Timestamp uploadedAt;
+
+    /**
+     * Instantiates a new Images.
+     */
+    public Images() {
+    }
+
+    /**
+     * Gets image id.
+     *
+     * @return the image id
+     */
+    public int getImage_id() {
+        return image_id;
+    }
+
+    /**
+     * Sets image id.
+     *
+     * @param image_id the image id
+     */
+    public void setImage_id(int image_id) {
+        this.image_id = image_id;
+    }
+
+    /**
+     * Gets project id.
+     *
+     * @return the project id
+     */
+    public int getProject_id() {
+        return project_id;
+    }
+
+    /**
+     * Sets project id.
+     *
+     * @param project_id the project id
+     */
+    public void setProject_id(int project_id) {
+        this.project_id = project_id;
+    }
+
+    /**
+     * Gets image url.
+     *
+     * @return the image url
+     */
+    public String getImage_url() {
+        return image_url;
+    }
+
+    /**
+     * Sets image url.
+     *
+     * @param image_url the image url
+     */
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    /**
+     * Gets uploaded at.
+     *
+     * @return the uploaded at
+     */
+    public Timestamp getUploadedAt() {
+        return uploadedAt;
+    }
+
+    /**
+     * Sets uploaded at.
+     *
+     * @param uploadedAt the uploaded at
+     */
+    public void setUploadedAt(Timestamp uploadedAt) {
+        this.uploadedAt = uploadedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Images{" +
+                "image_id=" + image_id +
+                ", project_id=" + project_id +
+                ", image_url='" + image_url + '\'' +
+                ", uploadedAt=" + uploadedAt +
+                '}';
+    }
+
+    /**
+     * Instantiates a new Images.
+     *
+     * @param project_id the project id
+     * @param image_url  the image url
+     * @param uploadedAt the uploaded at
+     */
+    public Images(int project_id, String image_url, Timestamp uploadedAt) {
+        this.project_id = project_id;
+        this.image_url = image_url;
+        this.uploadedAt = uploadedAt;
+    }
 }
--- src/main/java/entity/ProjectEntries.java
+++ src/main/java/entity/ProjectEntries.java
@@ -1,4 +1,117 @@
 package entity;
 
+import java.sql.Timestamp;
+
+/**
+ * The type Project entries.
+ */
 public class ProjectEntries {
+
+    private int id;
+
+    private int projectId;
+
+    private String entryText;
+
+    private Timestamp createdAt;
+
+    /**
+     * Instantiates a new Project entries.
+     */
+    public ProjectEntries() {
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "ProjectEntries{" +
+                "id=" + id +
+                ", projectId=" + projectId +
+                ", entryText='" + entryText + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+    /**
+     * Gets project id.
+     *
+     * @return the project id
+     */
+    public int getProjectId() {
+        return projectId;
+    }
+
+    /**
+     * Sets project id.
+     *
+     * @param projectId the project id
+     */
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    /**
+     * Gets entry text.
+     *
+     * @return the entry text
+     */
+    public String getEntryText() {
+        return entryText;
+    }
+
+    /**
+     * Sets entry text.
+     *
+     * @param entryText the entry text
+     */
+    public void setEntryText(String entryText) {
+        this.entryText = entryText;
+    }
+
+    /**
+     * Gets created at.
+     *
+     * @return the created at
+     */
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    /**
+     * Sets created at.
+     *
+     * @param createdAt the created at
+     */
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    /**
+     * Instantiates a new Project entries.
+     *
+     * @param projectId the project id
+     * @param entryText the entry text
+     * @param createdAt the created at
+     */
+    public ProjectEntries(int projectId, String entryText, Timestamp createdAt) {
+        this.projectId = projectId;
+        this.entryText = entryText;
+        this.createdAt = createdAt;
+    }
 }
