15b1318b03f3a2d6be1abcb24d1d284bea81bf18
pete.ladwig <pete.ladwig@singlewire.com>
2025-03-25 18:05:40 -0500

    adding delete project functionality

---
+++ src/main/java/controller/DeleteProject.java
@@ -0,0 +1,55 @@
+package controller;
+
+import entity.Project;
+import entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import persistence.GenericDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+
+@WebServlet("/deleteProject")
+public class DeleteProject extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(DeleteProject.class);
+    private static GenericDao<Project> projectDao = new GenericDao<>(Project.class);
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        Integer loggedInUserId = (Integer) session.getAttribute("userId");
+        String projectId = request.getParameter("projectId");
+        Project project = projectDao.getById(Integer.parseInt(projectId));
+        Integer projectUserId = (project.getUser().getId());
+
+        try {
+            if (!loggedInUserId.equals(projectUserId)) {
+                log.info("logged in user doesn't match project user id"); //TODO create some error pages ?
+                response.sendRedirect("error.jsp");
+                return;
+            }
+            projectDao.delete(project);
+            response.sendRedirect("error.jsp");
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendRedirect("error.jsp");
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String projectIdParam = request.getParameter("projectId");
+        Integer projectId = null;
+        projectId = Integer.parseInt(projectIdParam);
+        log.info("project id is {}", projectId);
+        Project project = projectDao.getById(projectId);
+        request.setAttribute("project", project);
+        request.getRequestDispatcher("/deleteProject.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/deleteProject.jsp
@@ -0,0 +1,17 @@
+<%@include file="head.jsp"%>
+<%@include file="taglib.jsp"%>
+
+
+<html>
+
+<body>
+    <h2>Confirm Project Deletion</h2>
+    <p>Are you sure you want to delete <strong>${project.name}</strong>?</p>
+    <form action="deleteProject" method="post">
+        <input type="hidden" name="projectId" value="${project.projectId}">
+        <input type="submit" value="Delete">
+        <a href="viewProject.jsp?projectId=${project.projectId}"><button type="button">Cancel</button></a>
+    </form>
+</body>
+
+</html>
