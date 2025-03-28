4e66a9cf23c9ab0a49c3b8b3e1faed07d80064ab
pete.ladwig <pete.ladwig@singlewire.com>
2025-03-28 09:24:34 -0500

    sick of some redundancy so starting a helper class for my servlets and adding a generic error page that i can pass a message in. Need to update previous work at some point

---
+++ src/main/java/util/ServletHelper.java
@@ -0,0 +1,38 @@
+package util;
+
+import entity.User;
+import persistence.GenericDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class ServletHelper {
+
+    private static GenericDao<User> userDao = new GenericDao<>(User.class);
+
+    /**
+     * gets logged in user object via session
+     * @param request
+     * @return
+     */
+    public static User getLoggedInUser(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        Integer userId = (Integer) session.getAttribute("userId");
+        return userDao.getById(userId);
+    }
+
+    /**
+     * sends user to error page with a note
+     * @param request
+     * @return
+     */
+    public static void sendToErrorPageWithMessage (HttpServletRequest request,  HttpServletResponse response, String message) throws ServletException, IOException {
+        request.setAttribute("message", message);
+        request.getRequestDispatcher("error.jsp").forward(request, response);
+    }
+
+
+}
---
+++ src/main/webapp/error.jsp
@@ -0,0 +1,14 @@
+<%@include file="head.jsp"%>
+<%@ page isErrorPage="true" %>
+<html>
+<head>
+    <title>Page Not Found</title>
+</head>
+<body>
+<div class="container">
+<h2>Whoops! Something went wrong</h2>
+<p>${message}</p>
+<a href="index.jsp">Go Home</a>
+</div>
+</body>
+</html>
