ef3c71d082f47bde9e750eb84ed022a22a53fb92
pete.ladwig <pete.ladwig@singlewire.com>
2025-03-30 14:13:57 -0500

    adding delete entry functionality

---
+++ src/main/java/controller/DeleteEntry.java
@@ -0,0 +1,54 @@
+package controller;
+
+import entity.Glaze;
+import entity.Project;
+import entity.ProjectEntry;
+import entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import persistence.GenericDao;
+import util.ServletHelper;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/deleteEntry")
+public class DeleteEntry extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(DeleteEntry.class);
+    private static GenericDao<Project> projectDao = new GenericDao<>(Project.class);
+    private static GenericDao<ProjectEntry> entryDao = new GenericDao<>(ProjectEntry.class);
+    private static ProjectEntry entry = new ProjectEntry();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int projectId = Integer.parseInt(request.getParameter("projectId"));
+        Project project = projectDao.getById(projectId);
+
+        if (ServletHelper.isLoggedInUserProjectOwner(request, project.getProjectId()) ) {
+            String entryIdParam = request.getParameter("entryId");
+            entry = entryDao.getById(Integer.parseInt(entryIdParam));
+            entryDao.delete(entry);
+            response.sendRedirect("viewProject?projectId=" + projectId);
+        }
+        else {
+            ServletHelper.sendToErrorPageWithMessage(request, response, "Permission denied - reach out to site administrator");
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String entryIdParam = request.getParameter("id");
+        entry = entryDao.getById(Integer.parseInt(entryIdParam));
+        int projectId = Integer.parseInt(request.getParameter("projectId"));
+        Project project = projectDao.getById(projectId);
+        request.setAttribute("entry", entry);
+        request.setAttribute("project", project);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/deleteEntry.jsp");
+        dispatcher.forward(request, response);
+    }
+}
---
+++ src/main/webapp/deleteEntry.jsp
@@ -0,0 +1,18 @@
+<%@include file="head.jsp"%>
+<%@include file="taglib.jsp"%>
+
+
+<html>
+
+<body>
+    <h2>Confirm Entry Deletion</h2>
+    <p>Are you sure you want to delete your project entry?</p>
+    <form action="deleteEntry" method="post">
+        <input type="hidden" name="entryId" value="${entry.id}">
+        <input type="hidden" name="projectId" value="${project.projectId}">
+        <input type="submit" value="Delete">
+        <a href="viewProject?${project.projectId}"><button type="button">Cancel</button></a>
+    </form>
+</body>
+
+</html>
