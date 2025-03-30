9efe37db4fb0d8f59abcf3191a0b8796a77e417f
pete.ladwig <pete.ladwig@singlewire.com>
2025-03-30 14:14:14 -0500

    adding edit entry funnctionality

---
+++ src/main/java/controller/EditEntry.java
@@ -0,0 +1,66 @@
+package controller;
+
+import entity.Project;
+import entity.ProjectEntry;
+import entity.Tag;
+import entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import persistence.GenericDao;
+import util.ServletHelper;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * A servlet to edit entry.
+ * @author pete
+ */
+
+@WebServlet("/editEntry")
+public class EditEntry extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(EditEntry.class);
+    private static GenericDao<ProjectEntry> entryDao = new GenericDao<>(ProjectEntry.class);
+    private static GenericDao<Project> projectDao = new GenericDao<>(Project.class);
+    private static ProjectEntry entry = new ProjectEntry();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String entryIdParam = request.getParameter("id");
+        int projectId = Integer.parseInt(request.getParameter("projectId"));
+        Project project = projectDao.getById(projectId);
+        entry = entryDao.getById(Integer.parseInt(entryIdParam));
+        request.setAttribute("entry", entry);
+        request.setAttribute("project", project);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/editEntry.jsp");
+        dispatcher.forward(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int projectId = Integer.parseInt(request.getParameter("projectId"));
+        Project project = projectDao.getById(projectId);
+
+
+        if (ServletHelper.isLoggedInUserProjectOwner(request, project.getProjectId()) ) {
+
+            String entryText = request.getParameter("entryText");
+            entry.setEntryText(entryText);
+
+            entryDao.saveOrUpdate(entry);
+            response.sendRedirect("viewProject?projectId=" + projectId);
+        }
+        else {
+            log.info("the project is {}", project);
+            ServletHelper.sendToErrorPageWithMessage(request, response, "Permission denied - reach out to site administrator");
+        }
+
+    }
+}
---
+++ src/main/webapp/editEntry.jsp
@@ -0,0 +1,28 @@
+<%@include file="head.jsp"%>
+<%@include file="taglib.jsp"%>
+
+<html>
+
+<body>
+    <div class="container">
+        <h1>Edit Tag</h1>
+
+        <c:choose>
+            <c:when test="${empty userName}">
+                <p>You must be logged in to edit an entry. <a href="index.jsp">Log in</a></p>
+            </c:when>
+            <c:otherwise>
+                <form action="editEntry" method="post">
+                    <div class="form-group">
+                        <label for="entryText">Edit blog entry:</label>
+                        <textarea id="entryText" name="entryText" rows="3" required>${entry.entryText}</textarea>
+                    </div>
+                    <input type="hidden" name="projectId" value="${project.projectId}">
+                    <button type="submit">Edit Entry</button>
+                    <a href="viewProject?${project.projectId}"><button type="button">Cancel</button></a>
+                </form>
+            </c:otherwise>
+        </c:choose>
+    </div>
+</body>
+</html>
