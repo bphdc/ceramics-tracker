d32f53623f96d8cfa5f41c1b93d8d32d4a6a0d37
pete.ladwig <pete.ladwig@singlewire.com>
2025-03-30 09:02:04 -0500

    adding edit tag functionality

---
+++ src/main/java/controller/EditTag.java
@@ -0,0 +1,58 @@
+package controller;
+
+import entity.Glaze;
+import entity.Tag;
+import entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import persistence.GenericDao;
+import util.ServletHelper;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * A servlet to add the glaze.
+ * @author pete
+ */
+
+@WebServlet("/editTag")
+public class EditTag extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(AddTag.class);
+    private static GenericDao<Tag> tagDao = new GenericDao<>(Tag.class);
+    private static Tag tag = new Tag();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String tagIdParam = request.getParameter("tagId");
+        tag = tagDao.getById(Integer.parseInt(tagIdParam));
+        request.setAttribute("tag", tag);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/editTag.jsp");
+        dispatcher.forward(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = ServletHelper.getLoggedInUser(request);
+
+        if (ServletHelper.isAdmin(user)) {
+
+            String name = request.getParameter("name");
+            tag.setName(name);
+
+            tagDao.saveOrUpdate(tag);
+            response.sendRedirect("tagLibrary");
+        }
+        else {
+            ServletHelper.sendToErrorPageWithMessage(request, response, "Permission denied - reach out to site administrator");
+        }
+
+    }
+}
---
+++ src/main/webapp/editTag.jsp
@@ -0,0 +1,26 @@
+<%@include file="head.jsp"%>
+<%@include file="taglib.jsp"%>
+
+<html>
+
+<body>
+    <div class="container">
+        <h1>Edit Tag</h1>
+
+        <c:choose>
+            <c:when test="${empty userName}">
+                <p>You must be logged in to edit a tag. <a href="login.jsp">Log in</a></p>
+            </c:when>
+            <c:otherwise>
+                <form action="editTag" method="post">
+                    <div class="form-group">
+                        <label for="name">Tag Name:</label>
+                        <input type="text" id="name" name="name" value="${tag.name}" required>
+                    </div>
+                    <button type="submit">Edit Tag</button>
+                </form>
+            </c:otherwise>
+        </c:choose>
+    </div>
+</body>
+</html>
